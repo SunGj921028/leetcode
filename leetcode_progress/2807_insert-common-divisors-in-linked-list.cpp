f22f502ed926642b366ceb0a90468cd7128c8cf4
SunGj921028 <sungj921028@gmail.com>
2024-09-10 22:22:11 +0800

    feat: Add problem 2807

---
+++ leetcode_progress/2807_insert-common-divisors-in-linked-list.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    int gcd(int a, int b){
+        if(b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        ListNode *temp = head;
+        while(temp != nullptr && temp->next != nullptr) {
+            int a = temp->val;
+            int b = temp->next->val;
+            int commonDivisor = gcd(a, b);
+            ListNode *newNode = new ListNode(commonDivisor);
+            newNode->next = temp->next;
+            temp->next = newNode;
+            temp = temp->next->next;
+        }
+        return head;
+    }
+};
+
+int main(){
+    Solution solution;
+    ListNode* head = new ListNode(18);
+    head->next = new ListNode(6);
+    head->next->next = new ListNode(10);
+    head->next->next->next = new ListNode(3);
+
+    head = solution.insertGreatestCommonDivisors(head);
+
+    // Print the resulting list
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+
+    return 0;
+}
