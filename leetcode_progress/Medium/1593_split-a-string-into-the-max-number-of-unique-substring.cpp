c9687ae2720c729e9b447b6f1d0d9f02fd53a00b
SunGj921028 <sungj921028@gmail.com>
2024-10-21 21:57:52 +0800

    feat: Add problem 1593

---
+++ leetcode_progress/Medium/1593_split-a-string-into-the-max-number-of-unique-substring.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxUniqueSplit(string s) {
+        unordered_set<string> seen;
+        return backtrack(0, s, seen);
+    }
+private:
+    int backtrack(int start, const string& s, unordered_set<string>& seen) {
+        if (start == s.size()) {
+            return 0;
+        }
+        int maxSplits = 0;
+        for (int end = start + 1; end <= s.size(); ++end) {
+            string substring = s.substr(start, end - start);
+            if (seen.find(substring) == seen.end()) {
+                seen.insert(substring);
+                maxSplits = max(maxSplits, 1 + backtrack(end, s, seen));
+                seen.erase(substring);
+            }
+        }
+        return maxSplits;
+    }
+};
+
+int main(){
+    Solution sol;
+    cout << sol.maxUniqueSplit("ababccc") << endl;
+    cout << sol.maxUniqueSplit("aba") << endl;
+    cout << sol.maxUniqueSplit("aa") << endl;
+    cout << sol.maxUniqueSplit("addbsd") << endl;
+    return 0;
+}
