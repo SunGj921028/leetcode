97c49ddb7ae98c65ba3b63783f4d03aad5b166fd
SunGj921028 <sungj921028@gmail.com>
2024-12-07 16:52:05 +0800

    feat: Add problem 1760

---
+++ leetcode_progress/Medium/1760_Minimum-limit-of-balls-in-a-bag.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int low = 1, high = *max_element(nums.begin(), nums.end());
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int ops = 0;
+            for (int n : nums) if ((ops += (n - 1) / mid) > maxOperations) break;
+            ops <= maxOperations ? high = mid : low = mid + 1;
+        }
+        return high;
+    }
+};
+
+int main(){
+    Solution s;
+    vector<int> nums = {9};
+    int maxOperations = 2;
+    cout << s.minimumSize(nums, maxOperations) << endl;
+    return 0;
+}
