5e555ec996a719bf9d3d6e0f611f21663059757f
SunGj921028 <sungj921028@gmail.com>
2024-12-10 13:50:14 +0800

    feat: Add problem 2981

---
+++ leetcode_progress/Medium/2981_Find-longest-special-substring-that-occurs-thrice-I.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool helper(const string& s, int n, int x) {
+        vector<int> cnt(26, 0);
+        int p = 0;
+
+        for (int i = 0; i < n; i++) {
+            while (s[p] != s[i]) p++;
+            if (i - p + 1 >= x) cnt[s[i] - 'a']++;
+            if (cnt[s[i] - 'a'] > 2) return true;
+        }
+
+        return false;
+    }
+
+    int maximumLength(string s) {
+        int n = s.size();
+        int l = 1, r = n;
+
+        if (!helper(s, n, l)) return -1;
+
+        while (l + 1 < r) {
+            int mid = (l + r) / 2;
+            if (helper(s, n, mid)) l = mid;
+            else r = mid;
+        }
+
+        return l;
+    }
+};
+
+int main(){
+    Solution sol;
+    string s = "aaaa";
+    cout << sol.maximumLength(s) << endl;
+    return 0;
+}
